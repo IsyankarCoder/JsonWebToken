74e9b96834ac2bdb6743f53522d1beadb1f2c666
Volkan Genç <libertymsdn55@hotmail.com>
2018-10-23 16:23:32 +0300

    Add project files.

---
+++ WebApplication12/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace WebApplication12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class BookController : ControllerBase
+    {
+
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<Book> Get()
+        {
+            int age=-1;
+            var currentUser = HttpContext.User;
+            var resultBooksList = new List<Book>
+            {
+               new Book { Author = "Ray Bradbury",Title = "Fahrenheit 451",AgeRestriction=false },
+               new Book { Author = "Gabriel García Márquez", Title = "One Hundred years of Solitude",AgeRestriction=true },
+               new Book { Author = "George Orwell", Title = "1984" ,AgeRestriction=true},
+               new Book { Author = "Anais Nin", Title = "Delta of Venus",AgeRestriction=false }
+            };
+
+            if (currentUser.HasClaim(c => c.Type == ClaimTypes.DateOfBirth))
+            {
+                DateTime BirthDate = DateTime.Parse(currentUser.Claims.FirstOrDefault(d => d.Type == ClaimTypes.DateOfBirth).Value);
+                age =DateTime.Today.Year - BirthDate.Year;
+            }
+
+
+            if (age < 18)
+            {
+                resultBooksList = resultBooksList.Where(b => !b.AgeRestriction).ToList();
+            }
+
+
+            return resultBooksList;
+        }
+
+
+        public class Book
+        {
+            public string Author { get; set; }
+            public string Title { get; set; }
+            public bool AgeRestriction { get; set; }
+
+        }
+    }
+}
---
+++ WebApplication12/Controllers/TokenController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+
+namespace WebApplication12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+
+        public IActionResult CreateToken([FromBody] LoginModel login)
+        {
+
+            IActionResult response = Unauthorized();
+            var user = CustomAuthenticate(login);
+            if (user != null)
+            {
+                var tokenstring = BuildToken(user);
+                response = new OkObjectResult(new { token = tokenstring });
+            }
+
+            return response;
+        }
+
+        private string BuildToken(UserModel userModel)
+        {
+
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub,userModel.Name),
+                new Claim(JwtRegisteredClaimNames.Email,userModel.Email),
+                new Claim(JwtRegisteredClaimNames.Birthdate,userModel.BirthDate.ToString("yyyy-MM-dd")),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                                            _config["Jwt:Issuer"],
+                                            claims,
+                                            expires: DateTime.Now.AddMinutes(30),
+                                            signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+        public UserModel CustomAuthenticate(LoginModel lgmodel)
+        {
+            UserModel userModel = null;
+            if (lgmodel.UserName == "vv" && lgmodel.PassWord == "123")
+                userModel = new UserModel() { Email = "gencvolkan@gmail.com", Name = "volkan", BirthDate = DateTime.Now };
+            return userModel;
+        }
+
+
+    }
+
+
+   
+
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+
+
+}
